e3c2a28c9797ed27e985b48c592fe58d98aa66de
SienaL2023 <xiaosiena@gmail.com>
2023-12-10 17:58:28 -0800

    data scrapping part 1

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import random
+import webbrowser
+import requests
+import bs4
+
+
+valid_letters = "qwertyuiopasdfghjklzxcvbnm"
 
 # hangman
 # guess the word game
@@ -9,6 +15,8 @@ import random
 #  if letter inpur exist in word, display letter instead of _____
 # else: tries -= 1
 # repeat until word is complete or out of tries
+# check for invalid entries
+# bigger word bank
 def hangman():
     wordbank = ["water", "cooler", "summer"]
     word = random.choice(wordbank)
@@ -18,7 +26,7 @@ def hangman():
     main = ""
     # chances is 7 bc for the while loop its greater than zero and you subtract everytime so you would be at zero
     # when they lose which doesnt work so just saying 1 = lose game over works better
-    chances = 7
+    chances = 10
 
     # this holds onto all the letters that were guessed
     guesses_made = ""
@@ -26,28 +34,44 @@ def hangman():
         # restarts the main so it doesnt get rly long w/ a bunch of ___
         main = ""
         guess = input("What is your guess?")
+        guess = guess.lower()
+        while guess not in valid_letters or (guess in guesses_made):
+            guess = input("Only single letter guesses please, no repeats! What is your guess? ")
+            guess = guess.lower()
         # adds to guesses made everytime
         guesses_made += guess
         if guess not in word:
             print("letter not in word!")
             chances -= 1
-            if chances == 6:
+            if chances == 9:
                 print("o")
-            elif chances == 5:
+            elif chances == 8:
                 print("\o")
-            elif chances == 4:
+            elif chances == 7:
                 print("\o/")
-            elif chances == 3:
+            elif chances == 6:
                 print("\o/")
                 print(" | ")
-            elif chances == 2:
+            elif chances == 5:
                 print("\o/")
                 print(" | ")
                 print("/")
-            elif chances == 1:
+            elif chances == 4:
                 print("\o/")
                 print(" | ")
                 print(" /\\")
+            elif chances == 3:
+                print("\o/\\")
+                print(" | ")
+                print(" /\\")
+            elif chances == 2:
+                print("/\o/\\")
+                print(" | ")
+                print(" /\\")
+            elif chances == 1:
+                print("/\o/\\")
+                print(" | ")
+                print("_/\\_")
         # runs through every letter in the word, for the first letter, if guessed (in guesses_made),
         # it will print, if not, itll print a _
         for letter in word:
@@ -56,10 +80,24 @@ def hangman():
             else:
                 main += "_"
         if main == word:
+            print(word)
             print("You won!")
             break
         print(main)
         if chances == 1:
+            print(word)
             print("game over!")
             break
-hangman()
+
+def get_random_words():
+    url = 'https://randomwordgenerator.com/'
+    response = requests.get(url)
+# html = hyper text machine language
+    if response.status_code == 200:
+        soup = bs4.BeautifulSoup(requests.text, 'html.parser')
+        # print(soup)
+        word_elements = soup.find_all("li", class_ = "support")
+        for words in word_elements:
+            print(words)
+# hangman()
+get_random_words()
